c4697a8f8512d14faea293f7e18e4d0b25cac936
Zaoth OS <fylluszophia@proton.me>
2026-08-08 23:21:01 -0300

    Update example.sh

--- files/scripts/example.sh
+++ files/scripts/example.sh
@@ -5,6 +5,10 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-# Your code goes here.
-echo 'This is an example shell script'
-echo 'Scripts here will run during build if specified in recipe.yml'
+git clone https://github.com/fyllus/bashlib
+
+cp -r bashlib/bash /usr/lib/
+
+git clone https://github.com/fyllus/sharefile
+
+cp -r sharefile/share /usr/
